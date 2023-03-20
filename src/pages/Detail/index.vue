b742714367488e2de9b05397b9a1f95b3504a029
沈荻 <2951546725@qq.com>
2023-03-20 21:29:34 +0800

    商品属性

--- src/pages/Detail/index.vue
+++ src/pages/Detail/index.vue
@@ -17,9 +17,9 @@
         <!-- 左侧放大镜区域 -->
         <div class="previewWrap">
           <!--放大镜效果-->
-          <Zoom />
+          <Zoom :skuImageList="skuInfo.skuImageList || []" />
           <!-- 小图列表 -->
-          <ImageList />
+          <ImageList :skuImageList="skuInfo.skuImageList || []" />
         </div>
         <!-- 右侧选择区域布局 -->
         <div class="InfoWrap">
@@ -64,29 +64,13 @@
           <div class="choose">
             <div class="chooseArea">
               <div class="choosed"></div>
-              <dl>
-                <dt class="title">选择颜色</dt>
-                <dd changepirce="0" class="active">金色</dd>
-                <dd changepirce="40">银色</dd>
-                <dd changepirce="90">黑色</dd>
-              </dl>
-              <dl>
-                <dt class="title">内存容量</dt>
-                <dd changepirce="0" class="active">16G</dd>
-                <dd changepirce="300">64G</dd>
-                <dd changepirce="900">128G</dd>
-                <dd changepirce="1300">256G</dd>
-              </dl>
-              <dl>
-                <dt class="title">选择版本</dt>
-                <dd changepirce="0" class="active">公开版</dd>
-                <dd changepirce="-1000">移动版</dd>
-              </dl>
-              <dl>
-                <dt class="title">购买方式</dt>
-                <dd changepirce="0" class="active">官方标配</dd>
-                <dd changepirce="-240">优惠移动版</dd>
-                <dd changepirce="-390">电信优惠版</dd>
+              <dl v-for="spuSaleAttr in spuSaleAttrList" :key="spuSaleAttr.id">
+                <dt class="title">{{ spuSaleAttr.saleAttrName }}</dt>
+                <dd changepirce="0" :class="{ active: spuvalue.isChecked == 1 }"
+                  v-for="spuvalue in spuSaleAttr.spuSaleAttrValueList" :key="spuvalue.id"
+                  @click="changeActive(spuvalue, spuSaleAttr.spuSaleAttrValueList)">{{
+                    spuvalue.saleAttrValueName }}</dd>
+
               </dl>
             </div>
             <div class="cartWrap">
@@ -362,7 +346,17 @@ export default {
     this.$store.dispatch('getGoodInfo', this.$route.params.skuid)
   },
   computed: {
-    ...mapGetters(['categoryView', 'skuInfo'])
+    ...mapGetters(['categoryView', 'skuInfo', 'spuSaleAttrList']),
+
+  },
+  methods: {
+    changeActive(saleAttrValue,arr) {
+    arr.forEach(element => {
+      element.isChecked=0
+    });
+    saleAttrValue.isChecked=1
+    }
+
   }
 
 }
